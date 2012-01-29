69badef9589a0642c8469f7ea57fa77ab80c73b2
Kyle S. Goodwin <kyle@devscoop.com>
2012-01-29 17:14:38 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buildmyvision::Application.config.secret_token = '56f568b6a0456bfb0bdb2ae38fd4b030b8680560f9f1786baeba023732489f368014dbde3f53c8eada7700dce9ee14e471e06d07a36d0d5dd585f1f092851789'
