87f712b4eeb67e402f8e0473e44553cb8439f282
Kyle S. Goodwin <kyle@devscoop.com>
2012-02-05 14:59:09 -0500

    Add robots.txt protections.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -6,4 +6,9 @@ class RootController < ApplicationController
 
   def about
   end
+
+  def robots
+    robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
+    render :text => robots, :layout => false, :content_type => "text/plain"
+  end
 end
